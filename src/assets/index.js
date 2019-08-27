2508ff1011c85c42612bae45c85c8214e9431178
serpentz <abyicherinet@gmail.com>
2019-08-26 21:47:37 -0400

    Removed conflicting css files that resulted in mobile view being skewed

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./images/logo/TiconLogoTextonside140by98.png"
-import logoWithNoText  from "./images/logo/ticonlogonotext266xby159px.png"
+import logoWithNoText from "./images/logo/ticonlogonotext266xby159px.png"
 import logotext from "./images/logo/TiconLogotextonly100x70.png"
-export {logo,logoWithNoText,logotext}
+import logoSVG from "./images/logo/icon.svg"
+export { logo, logoWithNoText, logotext, logoSVG }
--- src/components/Hero/Component.js
+++ src/components/Hero/Component.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import {logoWithNoText} from "../../assets"
+import {logoSVG} from "../../assets"
 function Hero() {
 	return (
 		<Fragment>
@@ -7,7 +7,7 @@ function Hero() {
 				<div className="slick-header fullheight">
 					<div className="header-slide slide-2">
 						<div className="container">
-							<img src={logoWithNoText} className="pull-left header-p" alt="P" />
+							<img src={logoSVG} className="pull-left header-p" alt="P" />
 							<p className="pre-title">Welcome to Ticon Labs</p>
 							<h2>We build amazing<br /><span>Software</span></h2>
 							<a href="#" className="header-button" tabIndex={-1}>View portfolio</a>
